631677743f42ae09e00da92e347ff53125a68c65
DemonStore <demonstoreby@gmail.com>
2015-01-28 20:17:50 +0300

    Initial commit

---
+++ android/src/com/bsu/mmf/web/course_work/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.bsu.mmf.web.course_work.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.bsu.mmf.web.course_work.OurGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new OurGame(), config);
+	}
+}
