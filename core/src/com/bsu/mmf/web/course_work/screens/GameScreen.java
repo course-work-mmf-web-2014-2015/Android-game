32428996c70c99ee3dbb195d5a54c6cd20ae2e0c
Anton <anton.shchennikov@gmail.com>
2015-02-06 13:41:41 +0300

    add GameScreen

---
+++ core/src/com/bsu/mmf/web/course_work/screens/GameScreen.java
@@ -0,0 +1,49 @@
+package com.bsu.mmf.web.course_work.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+
+/**
+ * Created by Anton on 06.02.2015.
+ */
+public class GameScreen implements Screen {
+
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+        Gdx.gl.glClearColor(10/255.0f, 15/255.0f, 230/255.0f, 1f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
